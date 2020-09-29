3b2bcf6917842b3e685a20ebce5a463bb3fe7293
yousef omar <yousefomar724@gmail.com>
2020-09-30 00:47:37 +0200

    added the main files

---
+++ main.js
@@ -0,0 +1,29 @@
+// menu show
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener("click", ()=>{
+            nav.classList.toggle("show");
+        })
+    }
+}
+
+showMenu("nav-toggle", "nav-menu")
+
+//////////////////////////////////////////////////////////
+// Remove menu from mobile view
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    // active link
+    navLink.forEach(n => n.classList.remove("active"));
+    this.classList.add("active");
+
+    // Rremove menu mobile
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show");
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction))
